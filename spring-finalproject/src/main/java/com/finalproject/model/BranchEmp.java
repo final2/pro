82310dafbe48206c5fc1437deeda1bec1d752c56
'boram' <yeyeya1008@gmail.com>
2016-09-21 10:52:28 +0900

    Branch Employee VO 생성

---
+++ spring-finalproject/src/main/java/com/finalproject/model/BranchEmp.java
@@ -0,0 +1,87 @@
+package com.finalproject.model;
+
+import java.util.Date;
+
+public class BranchEmp {
+
+	private int no;
+	private String email;
+	private String address;
+	private String phone;
+	private String grade;
+	private String password;
+	private String name;
+	private Date retireDate;
+	private Branch branch;
+	private String images;
+	private Date birth;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getRetireDate() {
+		return retireDate;
+	}
+	public void setRetireDate(Date retireDate) {
+		this.retireDate = retireDate;
+	}
+	public Branch getBranch() {
+		return branch;
+	}
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+	public String getImages() {
+		return images;
+	}
+	public void setImages(String images) {
+		this.images = images;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	
+	
+}
