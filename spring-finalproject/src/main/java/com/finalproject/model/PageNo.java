23b226dc79cbaf4c1c92f5e468671d3bbb63c3f0
'CYR' <c.y.roked@gmail.com>
2016-09-22 14:38:02 +0900

    mybatis-config.xml PageNO alias 추가

---
+++ spring-finalproject/src/main/java/com/finalproject/model/PageNo.java
@@ -0,0 +1,36 @@
+package com.finalproject.model;
+
+public class PageNo {
+
+	private int pageNo;
+	private int totalNo;
+	private int beginNo;
+	private int endNo;
+	
+	public int getPageNo() {
+		return pageNo;
+	}
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+	public int getTotalNo() {
+		return totalNo;
+	}
+	public void setTotalNo(int totalNo) {
+		this.totalNo = totalNo;
+	}
+	public int getBeginNo() {
+		return beginNo;
+	}
+	public void setBeginNo(int beginNo) {
+		this.beginNo = beginNo;
+	}
+	public int getEndNo() {
+		return endNo;
+	}
+	public void setEndNo(int endNo) {
+		this.endNo = endNo;
+	}
+	
+	
+}
