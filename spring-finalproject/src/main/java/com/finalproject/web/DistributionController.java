cf8042fb90f2e1ddd6425fa61b2471a8bc5189c3
ax5656 <ax5656@gmail.com>
2016-09-22 14:57:35 +0900

    mybatis에distribution.xml 추가

--- spring-finalproject/src/main/java/com/finalproject/web/DistributionController.java
+++ spring-finalproject/src/main/java/com/finalproject/web/DistributionController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DistributionController {
 
-	// 거래처 페이지
-	@RequestMapping("client.do")
-	public String client() {
-		return "companydistribution/client";
+	// 거래처 리스트 페이지
+	@RequestMapping("clientList.do")
+	public String clientList() {
+		return "companydistribution/clientList";
 	}
 	
-	// 거래처 등록페이지
-	@RequestMapping("clientdetail.do")
-	public String clientdetail() {
-		return "companydistribution/clientdetail";
+	// 거래처 수정 페이지
+	@RequestMapping("updateClient.do")
+	public String updateClient() {
+		return "companydistribution/updateClient";
+	}
+	
+	// 거래처 등록 페이지
+	@RequestMapping("addClient.do")
+	public String addClient() {
+		return "companydistribution/addClient";
 	}
 	
 }
