c2b66140e7a0ee6a2f3005b6c5c4ecb0557c5d53
'CYR' <c.y.roked@gmail.com>
2016-09-23 15:05:09 +0900

    todayplanDao 수정

--- spring-finalproject/src/main/java/com/finalproject/dao/TodayPlanDao.java
+++ spring-finalproject/src/main/java/com/finalproject/dao/TodayPlanDao.java
@@ -1,5 +1,17 @@
 package com.finalproject.dao;
 
+import java.util.List;
+
+import com.finalproject.model.TodayPlan;
+
 public interface TodayPlanDao {
 
+	//오늘의 할일 추가하기
+	void addTodayPlan(TodayPlan todayPlan);
+	//오늘의 할일 삭제하기
+	void removeTodayPlan(TodayPlan todayPlan);
+	//오늘의 할일 전체 조회하기
+	List<TodayPlan> getTodayPlanList();
+	//오늘의 할일 사원 번호별 조회하기
+	List<TodayPlan> getTodayPlanListByEmpNo(int empNo);
 }
