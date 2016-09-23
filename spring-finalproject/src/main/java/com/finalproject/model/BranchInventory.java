ba39031736079c7853152f858e5a9648212daae6
'boram' <yeyeya1008@gmail.com>
2016-09-23 17:56:33 +0900

    branchInventory vo 추가

---
+++ spring-finalproject/src/main/java/com/finalproject/model/BranchInventory.java
@@ -0,0 +1,37 @@
+package com.finalproject.model;
+
+public class BranchInventory {
+
+	private int no;
+	private int qty;
+	private int branchNo;
+	private int productNo;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getBranchNo() {
+		return branchNo;
+	}
+	public void setBranchNo(int branchNo) {
+		this.branchNo = branchNo;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	
+	
+	
+}
