9a44af78276b03668045597693821346d93f846e
'CYR' <c.y.roked@gmail.com>
2016-09-23 14:40:29 +0900

    todayplancontroller 수정

--- spring-finalproject/src/main/java/com/finalproject/json/TodayPlanController.java
+++ spring-finalproject/src/main/java/com/finalproject/json/TodayPlanController.java
@@ -1,5 +1,22 @@
-package com.finalproject.web;
+package com.finalproject.json;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.finalproject.dao.TodayPlanDao;
+import com.finalproject.service.TodayPlanService;
+
+@RestController
 public class TodayPlanController {
-
+	
+	@Autowired
+	private TodayPlanDao todayDao;
+	
+	@RequestMapping(value="/todayplan/", method=RequestMethod.GET )
+	public String todayPlan() {
+		return null;
+	}
 }
