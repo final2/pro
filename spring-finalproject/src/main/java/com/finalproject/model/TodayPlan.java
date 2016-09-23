623a4b25768a92c09aa8a10fb03616b18f7b5d81
'CYR' <c.y.roked@gmail.com>
2016-09-23 15:06:42 +0900

    todayPlan 관련 파일 수정

--- spring-finalproject/src/main/java/com/finalproject/model/TodayPlan.java
+++ spring-finalproject/src/main/java/com/finalproject/model/TodayPlan.java
@@ -1,5 +1,45 @@
 package com.finalproject.model;
 
+import java.util.Date;
+
 public class TodayPlan {
 
+	private int no;
+	private String title;
+	private String contents;
+	private Date regdate;
+	private int empNo;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	
+	
 }
