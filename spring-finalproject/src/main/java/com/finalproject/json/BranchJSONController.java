de5e72ef2b0cbd817294269c2a2339bc2b40a7bb
'boram' <yeyeya1008@gmail.com>
2016-09-27 12:49:30 +0900

    branch - branchJSONController 수정

--- spring-finalproject/src/main/java/com/finalproject/json/BranchJSONController.java
+++ spring-finalproject/src/main/java/com/finalproject/json/BranchJSONController.java
@@ -8,29 +8,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.finalproject.dao.BranchDao;
+import com.finalproject.model.BranchOrderDetail;
 import com.finalproject.model.LargeCategory;
 import com.finalproject.model.Product;
 import com.finalproject.model.SmallCategory;
+import com.finalproject.service.BranchService;
 
 @RestController
 public class BranchJSONController {
 	
-	@Autowired BranchDao branchDao;
+	@Autowired BranchService brService;
 	
+	// 전체 대분류 조회
 	@RequestMapping(value="/lcat/", method=RequestMethod.GET)
 	public List<LargeCategory> getAllLargeCats() {
-		return branchDao.getAllLargeCats();
+		return brService.getAllLargeCats();
 	}
 	
+	// 대분류 번호로 소분류 조회
 	@RequestMapping(value="/scat/{no}", method=RequestMethod.GET)
 	public List<SmallCategory> getSmallCatsByLargeNo(@PathVariable("no") int no) {
-		return branchDao.getSmallCatsByLargeNo(no);
+		return brService.getSmallCatsByLargeNo(no);
 	}
 	
+	// 소분류 번호로 물품 조회
 	@RequestMapping(value="/pt/{no}", method=RequestMethod.GET)
 	public List<Product> getProductsBySmallNo(@PathVariable("no") int no) {
-		return branchDao.getProductsBySmallNo(no);
+		return brService.getProductsBySmallNo(no);
+	}
+	
+	// 발주대기 주문 조회
+	@RequestMapping(value="/wor/", method=RequestMethod.GET)
+	public List<BranchOrderDetail> getWaitingOrder() {
+		return brService.getWaitingOrder();
 	}
 
+	// 발주대기 목록에서 삭제
+	@RequestMapping(value="/wor/{no}", method=RequestMethod.DELETE)
+	public List<BranchOrderDetail> deleteOrderDetailByNo(@PathVariable("no") int no) {
+		brService.deleteWaitingByOrderDetailNo(no);
+		return brService.getWaitingOrder();
+	}
+	
 }
