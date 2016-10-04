025c07ed51c86e4ac3f63fba93b3be183e7cc5d5
'qksrbtjr' <qksrbtjr111@naver.com>
2016-10-04 16:31:31 +0900

    마이바티스 수정관련.

---
+++ spring-finalproject/src/main/java/com/finalproject/model/PageMessage.java
@@ -0,0 +1,21 @@
+package com.finalproject.model;
+
+public class PageMessage {
+	
+	private int no;
+	private PageVo pageVo;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public PageVo getPageVo() {
+		return pageVo;
+	}
+	public void setPageVo(PageVo pageVo) {
+		this.pageVo = pageVo;
+	}
+
+}
