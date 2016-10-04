449f1822770767efcdd0c7b1c46996d331bdefd5
sswwldbs <sswwldbs@gmail.com>
2016-10-04 11:16:30 +0900

    AccountBook vo 추가

---
+++ spring-finalproject/src/main/java/com/finalproject/model/AccountBook.java
@@ -0,0 +1,92 @@
+package com.finalproject.model;
+
+import java.util.Date;
+
+public class AccountBook {
+
+	private int no;
+	private String paymentDate;
+	private int salary;
+	private int overtime = 0;
+	private int insureHealth = 0;
+	private int insureLonghealth = 0;
+	private int insureSocial = 0;
+	private int employeeInsure = 0;
+	private int empNo;
+	
+	public AccountBook() {}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getOvertime() {
+		return overtime;
+	}
+
+	public void setOvertime(int overtime) {
+		this.overtime = overtime;
+	}
+
+	public int getInsureHealth() {
+		return insureHealth;
+	}
+
+	public void setInsureHealth(int insureHealth) {
+		this.insureHealth = insureHealth;
+	}
+
+	public int getInsureLonghealth() {
+		return insureLonghealth;
+	}
+
+	public void setInsureLonghealth(int insureLonghealth) {
+		this.insureLonghealth = insureLonghealth;
+	}
+
+	public int getInsureSocial() {
+		return insureSocial;
+	}
+
+	public void setInsureSocial(int insureSocial) {
+		this.insureSocial = insureSocial;
+	}
+
+	public int getEmployeeInsure() {
+		return employeeInsure;
+	}
+
+	public void setEmployeeInsure(int employeeInsure) {
+		this.employeeInsure = employeeInsure;
+	}
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	
+	
+}
