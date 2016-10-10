1d93cba2a98d61d4ec7ec68e3d180b8b5f1b53f2
'qksrbtjr' <qksrbtjr111@naver.com>
2016-10-10 17:51:12 +0900

    VO추가

---
+++ spring-finalproject/src/main/java/com/finalproject/model/SalesBrPr.java
@@ -0,0 +1,21 @@
+package com.finalproject.model;
+
+public class SalesBrPr {
+	
+	private int no;
+	private int price;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
