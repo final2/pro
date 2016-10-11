fec5e1e88dcca92beaa0306e3dbc641e67aa4893
sswwldbs <sswwldbs@gmail.com>
2016-10-11 15:26:31 +0900

    출퇴근 기록 vo 추가

---
+++ spring-finalproject/src/main/java/com/finalproject/web/WorkTime.java
@@ -0,0 +1,76 @@
+package com.finalproject.web;
+
+import java.util.Date;
+
+import com.finalproject.model.Employee;
+
+public class WorkTime {
+
+	private int no;
+	private Date attendance;
+	private Date leaving;
+	private String late = null;
+	private String earlyleave = null;
+	private String overtime = null;
+	private Employee emp;
+	
+	public WorkTime() {}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public Date getAttendance() {
+		return attendance;
+	}
+
+	public void setAttendance(Date attendance) {
+		this.attendance = attendance;
+	}
+
+	public Date getLeaving() {
+		return leaving;
+	}
+
+	public void setLeaving(Date leaving) {
+		this.leaving = leaving;
+	}
+
+	public String getLate() {
+		return late;
+	}
+
+	public void setLate(String late) {
+		this.late = late;
+	}
+
+	public String getEarlyleave() {
+		return earlyleave;
+	}
+
+	public void setEarlyleave(String earlyleave) {
+		this.earlyleave = earlyleave;
+	}
+
+	public String getOvertime() {
+		return overtime;
+	}
+
+	public void setOvertime(String overtime) {
+		this.overtime = overtime;
+	}
+
+	public Employee getEmp() {
+		return emp;
+	}
+
+	public void setEmp(Employee emp) {
+		this.emp = emp;
+	}
+	
+	
+}
